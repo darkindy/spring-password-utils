7ad4a6a250195f9e1e1429f2daa4505fa611fe06
Andrei Pietrușel <andreipsl@yahoo.ro>
2018-09-16 15:33:08 +0300

    Core functions implemented

--- src/StandardPasswordEncoder.php
+++ src/StandardPasswordEncoder.php
@@ -1,89 +1,69 @@
 <?php
+
 namespace Darkindy\SpringPasswordUtils;
 
 /**
-*  
-*
-*  @author Andrei Pietrușel
-*/
-class StandardPasswordEncoder 
+ * Mimics the behaviour of Spring Security's Java class of the same name.
+ *
+ * @author Andrei Pietrușel
+ */
+class StandardPasswordEncoder
 {
-	
-	/**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-	protected $m_SaltGenerator;
-	
-	public function __construct()
-	{
-		$this->m_SaltGenerator = new SaltGenerator;
-	}
-	
-	/**
-	* 
-	*
-	* @param string $param1 A string containing the parameter
-	*
-	* @return boolean
-	*/
-	public function matches($rawPassword, $encodedPassword)
-	{
-		$digested = $this->decode($encodedPassword);
-		$salt = $this->subArray($digested, 0, $this->m_SaltGenerator->getKeyLength());
-		return $this->matchesInternal($digested, $this->digest($rawPassword, $salt));
-	}
-	
-	protected function matchesInternal($expected, $actual)
-	{
-		if (strlen($expected) !== strlen($actual)) {
-            return false;
+
+    private const HASH_ALGORITHM = 'SHA256';
+    private const HASH_ITERATIONS = 1024;
+    private const HASH_LENGTH = 80;
+
+    /**
+     * Checks if the given password matches a previously computed encoded password, thus validating authentication.
+     * The encoded password is usually taken from the password column in the database.
+     *
+     * @param string $rawPassword The password to validate.
+     * @param string $encodedPassword The encoded password hash, which includes salt prefix.
+     * @param int $saltPrefixLength The length of the salt prefix of the encoded password. Defaults to 8.
+     *
+     * @return boolean
+     */
+    public function matches($rawPassword, $encodedPassword, $saltPrefixLength = 8)
+    {
+        $decodedDigest = hex2bin($encodedPassword);
+        $salt = substr($decodedDigest, 0, $saltPrefixLength);
+        $computedDigest = $this->digest($rawPassword, $salt);
+        return $decodedDigest === $computedDigest;
+    }
+
+    /**
+     * Encodes a password in the same way as Spring Security's StandardPasswordEncoder with default settings.
+     *
+     * @param $rawPassword The password to encode.
+     * @param $salt Optional salt used for encoding the password.
+     *              For test-proven results use a random length 8 ASCII string. If not specified it will be auto-provided.
+     * @return string The encoded password, ready to be stored in the database.
+     */
+    public function encode($rawPassword, $salt = null)
+    {
+        if (is_null($salt)) {
+            $salt = bin2hex(openssl_random_pseudo_bytes(4));
         }
-        $result = 0;
-        for ($i = 0; $i < strlen($expected); $i++) {
-            $result |= ord($expected[$i]) ^ ord($actual[$i]);
+        $computedDigest = $this->digest($rawPassword, $salt);
+        return bin2hex($computedDigest);
+    }
+
+    private function digest($rawPassword, $salt)
+    {
+        $utf8RawPassword = mb_convert_encoding($rawPassword, "UTF-8");
+        $digest = $this->hash($utf8RawPassword, $salt);
+        return $salt . $digest;
+    }
+
+    private function hash($password, $salt)
+    {
+        $password = $salt . $password;
+        for ($j = 0; $j < self::HASH_ITERATIONS; $j++) {
+            $password = hash(self::HASH_ALGORITHM, $password, true);
         }
-        return $result === 0;
-	}
-	
-	/**
-	* Extract a sub array of characters out of the char array.
-    * @param array $array The char array to extract from
-    * @param int $beginIndex The beginning index of the sub array, inclusive
-    * @param int $endIndex the ending index of the sub array, exclusive
-	* @return array The array containing the extracted subset of characters
-	*/
-	protected function subArray($array, $beginIndex, $endIndex)
-	{
-		$length = $endIndex - $beginIndex;
-        $subarray = array();
-		for($i = $beginIndex; ($i - $beginIndex) < $length; $i++) {
-			$subarray[$i - $beginIndex] = $array[$i];
-		}
-        return $subarray;
-	}
-	
-	protected function digest($rawPassword, $salt)
-	{
-		return $rawPassword;
-	}
-	
-	protected function decode($encodedPassword)
-	{
-		return Hex::decode($encodedPassword);
-	}
-	
-}
+        return $password ? substr($password, 0, self::HASH_LENGTH) : $password;
+    }
 
-class SaltGenerator {
-	public function getKeyLength()
-	{
-		return 8;
-	}
 }
-
-class Hex {
-	public static function decode($encodedString)
-	{
-		return $encodedString;
-	}
-}
-
 
--- tests/StandardPasswordEncoderTest.php
+++ tests/StandardPasswordEncoderTest.php
@@ -1,33 +1,58 @@
 <?php
+
+namespace Darkindy\SpringPasswordUtils;
+
 use PHPUnit\Framework\TestCase;
-use Darkindy\SpringPasswordUtils\StandardPasswordEncoder;
 
 /**
-*
-*
-*  @author Andrei Pietrușel
-*/
+ * Tests for StandardPasswordEncoder utility
+ *
+ * @author Andrei Pietrușel
+ */
 class StandardPasswordEncoderTest extends TestCase
 {
+    private const ENCODED_PASSWORD = "1ee070812605db4ab1950813a042717eb3f0bdf8395bac71d3eb9b8eeefcd5e86626731b2d41c864";
+    private const RAW_PASSWORD = "1234";
 
-	/**
-	* 
-	*/
-	public function testIsThereAnySyntaxError()
-	{
-		$var = new StandardPasswordEncoder;
-		$this->assertTrue(is_object($var));
-		unset($var);
-	}
-	
-	/**
-	* 
-	*/
-	public function testMatches()
-	{
-		$var = new StandardPasswordEncoder;
-		$this->assertTrue($var->matches("1234567890","1234567890") === true);
-		unset($var);
-	}
-  
+    private const PASSWORD_TO_ENCODE = "Darkindyß";
+    private const SALT_TO_ENCODE = "1BaHku&7";
+    private const EXPECTED_ENCODED_PASSWORD = "314261486b752637c6b53e6df54f937d229224965d58548a53beba5c4fc001083a5786aaac9cd448";
+
+    public function testIsThereAnySyntaxError()
+    {
+        $var = new StandardPasswordEncoder;
+        $this->assertTrue(is_object($var));
+        unset($var);
+    }
+
+    public function testMatches()
+    {
+        $var = new StandardPasswordEncoder;
+        $this->assertTrue($var->matches(self::RAW_PASSWORD, self::ENCODED_PASSWORD) === true);
+        unset($var);
+    }
+
+    public function testEncode()
+    {
+        $var = new StandardPasswordEncoder;
+        $this->assertTrue($var->encode(self::PASSWORD_TO_ENCODE, self::SALT_TO_ENCODE) === self::EXPECTED_ENCODED_PASSWORD);
+        unset($var);
+    }
+
+    public function testEncodeThenMatches()
+    {
+        $var = new StandardPasswordEncoder;
+        $encodedPassword = $var->encode(self::PASSWORD_TO_ENCODE, self::SALT_TO_ENCODE);
+        $this->assertTrue($var->matches(self::PASSWORD_TO_ENCODE, $encodedPassword) === true);
+        unset($var);
+    }
+
+    public function testEncodeThenMatchesAutoSalt()
+    {
+        $var = new StandardPasswordEncoder;
+        $encodedPassword = $var->encode(self::PASSWORD_TO_ENCODE);
+        $this->assertTrue($var->matches(self::PASSWORD_TO_ENCODE, $encodedPassword) === true);
+        unset($var);
+    }
+
 }
